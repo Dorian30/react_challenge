7286cf997884c19c11f22750061774bd65793c57
Dorian30 <alvaradogdorian@gmail.com>
2021-05-01 20:35:17 -0300

    Implemented routing inside the dashboard component for rendering each tab when requested by the user

--- src/pages/dashboard/DashBoardPage.js
+++ src/pages/dashboard/DashBoardPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
@@ -10,12 +11,19 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PeopleIcon from '@material-ui/icons/People';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
 import useStyles from "./styleHomePage";
 import UsersPaper from "./components/UsersPaper";
 import TODOPaper from "./components/TODOPaper";
 
 export default function DashBoardPage() {
     const classes = useStyles();
+    const history = useHistory();
 
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
@@ -45,21 +53,32 @@ export default function DashBoardPage() {
             >
                 <div className={classes.toolbarIcon}>
                 </div>
+                <List>
+                  <ListItem button onClick={() => history.push('/users')}>
+                    <ListItemIcon>
+                      <PeopleIcon />
+                    </ListItemIcon>
+                    <ListItemText>Usuarios</ListItemText>
+                  </ListItem>
+                  <ListItem button onClick={() => history.push('/todos')}>
+                    <ListItemIcon>
+                      <PlaylistAddCheckIcon />
+                    </ListItemIcon>
+                    <ListItemText>ToDo</ListItemText>
+                  </ListItem>
+                </List>
             </Drawer>
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
                     <Grid container spacing={3}>
-                        {/* Users */}
                         <Grid item xs={12} md={12} lg={12}>
                             <Paper className={fixedHeightPaper}>
-                                <UsersPaper />
-                            </Paper>
-                        </Grid>
-                        {/* ToDoPage */}
-                        <Grid item xs={12} md={12} lg={12}>
-                            <Paper className={fixedHeightPaper}>
-                                <TODOPaper />
+                              <Switch>
+                                <Route exact path="/user" component={UsersPaper} />
+                                <Route exact path="/todos" component={TODOPaper} />
+                                <Route render={() => <Redirect to="/user" />} />
+                              </Switch>
                             </Paper>
                         </Grid>
                     </Grid>
